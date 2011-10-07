78f87c3b9680f3e9010162ba3c65684a512f2050
Gautam Kumar <gautham5678@gmail.com>
2011-10-07 14:09:57 +0530

    First Commit with a program to find the position of a character in a string

---
+++ CharacterPostionInString.c
@@ -0,0 +1,45 @@
+/* 
+ * File:   main.c
+ * Author: gautam
+ *
+ * Created on 7 October, 2011, 1:40 PM
+ */
+
+#include <stdio.h>
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    char str[100] , c ;
+    int i;
+    printf ("Enter a string :");gets(str);
+    printf("Enter the character to find:");
+    c = getchar();
+    
+    for(i=0;
+            /*
+             * Loop until a Null character ( \0 ) is found in the string
+             * Remember an array is also a  pointer
+             */
+            *(str+i) != '\0' ;
+            i++)
+    {
+        
+        if( *(str+i) == c ){
+            /*
+             * If the Character is found Print its position and terminate the program
+             */
+            printf("The character was found at : %d",i);
+            return 1;
+        }
+    }
+    /*
+     * If character was not found print error;
+     */
+    printf("The character was not found in the string ");
+    return 0;
+}
+
---
+++ build/Debug/GNU-Linux-x86/_ext/279880100/POS.o.d
@@ -0,0 +1,1 @@
+build/Debug/GNU-Linux-x86/_ext/279880100/POS.o: ../../Desktop/POS.C
