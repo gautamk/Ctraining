06b545b7f9c6d5a57252478662abed6ef3654d1e
Gautam Kumar <gautham5678@gmail.com>
2011-10-07 19:49:44 +0530

    Converted to the library function format

--- CharacterPostionInString.c
+++ CharacterPostionInString.c
@@ -1,5 +1,4 @@
 /* 
- * File:   main.c
  * Author: gautam
  *
  * Created on 7 October, 2011, 1:40 PM
@@ -9,37 +8,31 @@
 
 
 /*
- * 
+ * Program to find the position of a character in a string
  */
-int main(int argc, char** argv) {
-    
-    char str[100] , c ;
+int CharPositionInString(char *str,char c) {
     int i;
-    printf ("Enter a string :");gets(str);
-    printf("Enter the character to find:");
-    c = getchar();
-    
     for(i=0;
             /*
              * Loop until a Null character ( \0 ) is found in the string
              * Remember an array is also a  pointer
              */
-            *(str+i) != '\0' ;
-            i++)
+            *(str+i) != '\0';
+            i++ )
     {
         
         if( *(str+i) == c ){
             /*
-             * If the Character is found Print its position and terminate the program
+             * If the Character is found return its position and terminate the program
              */
-            printf("The character was found at : %d",i);
-            return 1;
+            
+            return i;
         }
     }
     /*
-     * If character was not found print error;
+     * If character was not found return -1;
      */
-    printf("The character was not found in the string ");
-    return 0;
+
+    return -1;
 }
 
