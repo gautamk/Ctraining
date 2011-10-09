74d6730892d08d438b08757d7bc6b3fb444a55c3
Gautam Kumar <gautham5678@gmail.com>
2011-10-09 06:52:10 +0530

    NetBeans Project Reconstruction

--- main.c
+++ main.c
@@ -1,22 +1,17 @@
 /* 
- * File:   Main.c
+ * File:   main.c
  * Author: gautam
  *
- * Created on 7 October, 2011, 7:44 PM
+ * Created on 9 October, 2011, 6:50 AM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "Automobile.h"
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    
-    automobile_init(20);
-    
 
     return (EXIT_SUCCESS);
 }
